8fb1eda79f28deb7d67dda5b2c6b71ae9844b4d3
Brian Nicholas <uibri@me.com>
2014-06-03 15:31:51 -0700

    hmm

--- api.js
+++ api.js
@@ -5,8 +5,6 @@ var port    = process.env.PORT || 5000;
 
 app.listen(port, function() { console.log("Listening on " + port) });
 
-app.use(express.static(path.join(__dirname, 'app')));
-
 var getOptions = require('./searchOptions.js');
 var getAuctions = require('./searchAuctions.js');
 app.get('/options', getOptions.searchOptions);
