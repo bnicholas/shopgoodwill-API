29c2236755b06edf1952cac3b36d9a92acd58a98
Brian Nicholas <uibri@me.com>
2016-05-18 06:38:04 -0700

    forgot cors module

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var app     = express();
 var port    = process.env.PORT || 8080;
 var os      = require('os');
 
-app.listen(port, function() { console.log("Listening on " + port) });
-
-app.use(allowCrossDomain);
-
 var getSellers    = require('./getSellers.js');
 var getCategories = require('./getCategories.js');
 var getAuctions   = require('./getAuctions.js');
 var getItem       = require('./getItem.js');
+
+var cors = require('cors');
+app.use(cors());
 
 app.get('/', getAuctions.listAuctions);
 app.get('/auctions', getAuctions.listAuctions);
 app.get('/categories', getCategories.listCategories);
 app.get('/item', getItem.showAuction);
 app.get('/sellers', getSellers.listSellers);
+
+app.listen(port, function() { console.log("Listening on " + port) });
